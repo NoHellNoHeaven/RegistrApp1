fc8647d469e983946b9a9555781615c652e2eeaf
NoHellNoHeaven <espindolaalex627@gmail.com>
2024-11-25 12:49:30 -0300

    hola

--- src/app/estudiante/nueva-asistencia/nueva-asistencia.page.ts
+++ src/app/estudiante/nueva-asistencia/nueva-asistencia.page.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { CapacitorBarcodeScanner } from '@capacitor/barcode-scanner';
-import { Platform } from '@ionic/angular';
-import { Router } from '@angular/router';
+import { CapacitorBarcodeScanner, CapacitorBarcodeScannerTypeHint } from '@capacitor/barcode-scanner';
 
 @Component({
   selector: 'app-nueva-asistencia',
@@ -9,29 +7,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./nueva-asistencia.page.scss'],
 })
 export class NuevaAsistenciaPage {
-  constructor(private platform: Platform, private router: Router) {}
-
-  async scanQRCode() {
-    if (this.platform.is('capacitor')) {
-      const status = await CapacitorBarcodeScanner.checkPermission({ force: true });
-      if (status.granted) {
-        await CapacitorBarcodeScanner.hideBackground(); // Oculta la vista web para una mejor experiencia de escaneo
-
-        const result = await CapacitorBarcodeScanner.startScan(); // Inicia el escaneo
-
-        if (result.hasContent) {
-          console.log('Código QR escaneado:', result.content);
-          // Aquí puedes agregar la lógica para registrar la asistencia
-        } else {
-          console.log('No se encontró contenido en el código QR');
-        }
-
-        await CapacitorBarcodeScanner.showBackground(); // Muestra la vista web nuevamente
-      } else {
-        console.log('Permiso de cámara no concedido');
-      }
-    } else {
-      console.log('El escaneo de códigos QR solo está disponible en dispositivos móviles');
-    }
+  toast: any;
+  async scan(){
+    CapacitorBarcodeScanner.scanBarcode({hint: CapacitorBarcodeScannerTypeHint.ALL})
+    .then((data)=>{
+      this.showToast(data.ScanResult)
+    
+    })
   }
+
+  async showToast(texto: string) {
+    const toast = await this.toast.create({
+      message: texto,
+      duration: 3000,
+      positionAnchor: 'footer2',
+      cssClass: 'rounded-toast'
+    });
+    await toast.present();
+  }
+  
 }
